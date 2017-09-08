576502995a4283eb57f22fa4d8b17c8f8760686b
toneloke <guerrerotonyjs@gmail.com>
2017-09-08 13:44:05 -0400

    finished hangman

--- __test__/hangman.test.js
+++ __test__/hangman.test.js
@@ -2,35 +2,34 @@ import Hangman from '../hangman' // => import for one item only
 // import { Hangman, Main } from '../hangman' //object destructoring
 
 describe('Hangman Class', () => {
+  let myGame
+
+  beforeEach(() => {
+    myGame = new Hangman('Superman')
+  })
+
   test('Hangman constructor exist', () => {
-    let myGame = new Hangman()
     expect(myGame.constructor).toBeDefined()
   })
   test('Hangman instance has a property called objective', () => {
-    let myGame = new Hangman('superman')
     expect(myGame).toHaveProperty('objective', 'superman')
   })
   test('Guess a letter on objective to be true', () => {
-    let myGame = new Hangman('superman')
     expect(myGame.guess('s')).toBeTruthy()
   })
   test('Guess a letter on objective to be false', () => {
-    let myGame = new Hangman('superman')
     expect(myGame.guess('i')).toBeFalsy()
   })
   test('Guess input is a single character', () => {
-    let myGame = new Hangman('superman')
     expect(myGame.guess('si')).toMatch('you can only guess one letter at a time')
   })
   test('keep track of the number of incorrect guesses', () => {
-    let myGame = new Hangman('superman')
     myGame.guess('s')
     myGame.guess('i')
     myGame.guess('l')
     expect(myGame.numOfGuesses).toEqual(2)
   })
   test('loose the game if guesses are greater than 6', () => {
-    let myGame = new Hangman('superman')
     myGame.guess('v')
     myGame.guess('i')
     myGame.guess('l')
@@ -39,7 +38,6 @@ describe('Hangman Class', () => {
     expect(myGame.guess('b')).toMatch('x.x')
   })
   test('did the player figure out the word', () => {
-    let myGame = new Hangman('superman')
     myGame.guess('s')
     myGame.guess('i')
     myGame.guess('u')
@@ -50,8 +48,37 @@ describe('Hangman Class', () => {
     myGame.guess('n')
     expect(myGame.guess('a')).toMatch('you win')
   })
-
-  // will not allow numbers
-  // is not case sensative
+  test('argument passed to guess() is not a number', () => {
+    expect(myGame.guess(1)).toMatch('does not accept a number as an argument')
+  })
+  test('word argument is not case sensative', () => {
+    expect(myGame.guess('S')).toBeTruthy()
+    expect(myGame.guess('I')).toBeFalsy()
+  })
   // guess the whole word
+  test('player wants to guess the entire word', () => {
+    expect(myGame.guess('SUPERMAN')).toMatch('you win')
+    expect(myGame.guess('superguy')).toBeFalsy()
+  })
+  test('last guess is a word and player looses', () => {
+    myGame.guess('v')
+    myGame.guess('i')
+    myGame.guess('l')
+    myGame.guess('w')
+    myGame.guess('q')
+    expect(myGame.guess('superguy')).toMatch('you loose')
+  })
+  test('can guess word with duplicate characters', () => {
+    let gameTwo = new Hangman('apple')
+    gameTwo.guess('p')
+    gameTwo.guess('a')
+    gameTwo.guess('l')
+    expect(gameTwo.guess('e')).toMatch('you win')
+    let gameThree = new Hangman('mississippi')
+    gameThree.guess('m')
+    gameThree.guess('s')
+    gameThree.guess('i')
+    gameThree.guess('mississipi')
+    expect(gameThree.guess('p')).toMatch('you win')
+  })
 })
--- hangman.js
+++ hangman.js
@@ -8,19 +8,37 @@
 
 class Hangman {
   constructor (word) {
-    this.objective = word
+    this.objective = word.toLowerCase()
     this.numOfGuesses = 0
     this.correctGuesses = 0
   }
   guess (letter) {
+    if (typeof letter === 'number' && typeof letter !== 'string') return 'does not accept a number as an argument'
+    let letterLC = letter.toLowerCase()
+    if (letterLC.length === this.objective.length) {
+      if (letterLC === this.objective) {
+        return 'you win!'
+      } else {
+        this.numOfGuesses++
+        return this.numOfGuesses >= 6 ? 'you loose' : false
+      }
+    }
     if (letter.length > 1) return 'you can only guess one letter at a time'
-    let isMatch = this.objective.includes(letter)
-    if (isMatch) {
-      this.correctGuesses++
+
+    let count = 0
+    let isMatch = false
+    this.objective.split('').forEach(e => {
+      if (e === letterLC) {
+        count++
+      }
+    })
+    if (count > 0) {
+      this.correctGuesses += count
+      isMatch = true
     } else {
       this.numOfGuesses++
     }
-
+    // console.log('number of correct guesses', this.correctGuesses)
     if (this.numOfGuesses >= 6) return 'x.x you loose'
     if (this.correctGuesses == this.objective.length) return ':) you win!'
     return isMatch
